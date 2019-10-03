ec315e4d9b777d3613a89f576e77e68d17addcd8
timary13 <timary99@gmail.com>
2019-10-03 21:53:05 +0300

    feat: add not working solution

--- src/index.js
+++ src/index.js
@@ -4,7 +4,59 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    console.log(expr);
+    findBrackets();
+
+    //find brackets
+    function findBrackets() {
+        let openBracketIndex = expr.lastIndexOf('(');
+        let closeBracketIndex = expr.indexOf(')');
+        //without one bracket from pair (!!(-1)->bool)
+        if ((!!closeBracketIndex && !(!!openBracketIndex)) || (!!openBracketIndex && !(!!closeBracketIndex))) {
+            console.log("here");
+            throw ("ExpressionError: Brackets must be paired");
+        }
+        plus(expr.slice(openBracketIndex + 1, closeBracketIndex));
+    }
+
+    function plus(str) {
+        console.log("plus "+str);
+        return str.split('+').map(item => minus(item))
+            .reduce(function (acum, item) {
+                return parseFloat(acum) + parseFloat(item);
+            });
+    }
+
+    function minus(str) {
+        console.log("minus "+str);
+        return str.split('-').map(item => divide(item))
+            .reduce(function (acum, item) {
+                return (parseFloat(acum) - parseFloat(item));
+            });
+    }
+
+    function divide(str) {
+        console.log("divide "+str);
+        return str.split('/').map(function (item) {
+            if (parseFloat(item) == 0)
+                throw ("TypeError: Division by zero.");
+            else {
+                multiply(item);
+            }
+        }).reduce(function (acum, item) {
+            console.log("acum "+ acum + " item " + item);
+            console.log("divide "+parseFloat(acum) / parseFloat(item));
+            return (parseFloat(acum) / parseFloat(item));
+        });
+    }
+
+    function multiply(str) {
+        console.log("multiply "+str);
+        return str.split('*').reduce(function (acum, item) {
+            console.log("multiply "+parseFloat(acum) * parseFloat(item));
+            return (parseFloat(acum) * parseFloat(item));
+        });
+    }
 }
 
 module.exports = {
